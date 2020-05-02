bcb042c1a51abcb6ff12020a12d1c1a1b25fb5cd
CISPL-ADMIN <cispl-admin@CISPL-IO-57.local>
2020-05-03 00:31:47 +0530

    2nd Commit

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ function autoemail_init(){
     echo "<h1>Test</h1>";
 };
 function autoemail_form_init(){
-    _e("<h1>Lists</h1>");
+    _e("<h1>Form</h1>");
 };
 function autoemail_list_init(){
     _e("<h1>Lists</h1>");
